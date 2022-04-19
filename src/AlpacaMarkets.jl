39a4b53bc6b89af0bb094a1c291ac4b1d3e417c4
dm13450 <dean.markwick@talk21.com>
2022-04-19 18:51:07 +0100

    Add ENV for sleep time

--- src/AlpacaMarkets.jl
+++ src/AlpacaMarkets.jl
@@ -4,9 +4,12 @@ module AlpacaMarkets
   using Dates
 
   const HEADERS = Ref{Vector{Pair{String, String}}}()
+  const SLEEP_TIME = Ref{Float64}()
 
   function __init__()
     auth()
+    SLEEP_TIME[] = tryparse(Float64, get(ENV, "ALPACA_SLEEP", "0.301"))
+    SLEEP_TIME[] = isnothing(SLEEP_TIME[]) ? 0.301 : SLEEP_TIME[]
   end
 
   function auth(api_key, api_secret)
