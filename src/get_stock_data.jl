4beb2de87602ae4d45addd3090782556ec535cd9
Marius Kruger <amanic@gmail.com>
2023-02-13 22:18:02 +0200

    update README.md and remove redundant utils

--- src/get_stock_data.jl
+++ src/get_stock_data.jl
@@ -16,12 +16,3 @@ get_stock_quotes(symbol, startTime, endTime, feed=nothing) = get_stock_data(stoc
 get_stock_trades(symbol, startTime, endTime, feed=nothing) = get_stock_data(stock_trades, symbol, startTime, endTime, feed=feed)
 
 export get_stock_quotes, get_stock_trades
-
-function get_stock_data_latest(f, symbol; feed=nothing)
-  f(symbol; feed = feed)
-end
-
-get_stock_quotes_latest(symbol, feed=nothing) = get_stock_data_latest(stock_quotes_latest, symbol, feed=feed)
-get_stock_trades_latest(symbol, feed=nothing) = get_stock_data_latest(stock_trades_latest, symbol, feed=feed)
-
-export get_stock_quotes_latest, get_stock_trades_latest
