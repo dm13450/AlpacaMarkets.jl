526e2a84838e0185cf3021d455220bc67c801981
Andrew Bannerman <bannerman1985@gmail.com>
2023-04-19 21:36:20 -0500

    Differentiate ENV and user input auth()

--- src/AlpacaMarkets.jl
+++ src/AlpacaMarkets.jl
@@ -10,32 +10,49 @@ module AlpacaMarkets
   const HEADERS = Ref{Vector{Pair{String, String}}}()
   const SLEEP_TIME = Ref{Float64}()
 
+  # added auth_startup() 
+  # non clashing function name
+  # this will first check for ENV keys else revert to user input
   function __init__()
-    auth()
+    auth_startup()
     SLEEP_TIME[] = tryparse(Float64, get(ENV, "ALPACA_SLEEP", "0.301"))
     SLEEP_TIME[] = isnothing(SLEEP_TIME[]) ? 0.301 : SLEEP_TIME[]
   end
 
+
+  # main function for setting keys 
+  # inputs 
+  # api_key :: is taken from ENV or user input 
+  # api_secret :: is taken from ENV or user input
   function auth(api_key, api_secret)
     HEADERS[] = ["APCA-API-KEY-ID" => api_key,
                  "APCA-API-SECRET-KEY" => api_secret]
   end
 
-  function auth()
-    api_key = get(ENV, "ALPACA_KEY", "")
-    api_secret = get(ENV, "ALPACA_SECRET", "")
+  # set to run on start up 
+  # will check ENV for keys else revert to user input
+  function auth_startup()
+    api_key_env = get(ENV, "ALPACA_KEY", "")
+    api_secret_env = get(ENV, "ALPACA_SECRET", "")
  
-    if api_key == "" 
-      @warn "API key details not found, authenticate manuallly with auth()"
-      return
+    if api_key_env == "" 
+      @warn "API key details not found, authenticate manuallly with auth() \n"
     end
 
-    if api_secret == ""
-      @warn "API secret details not found, authenticate manuallly with auth()"
-      return
+    if api_secret_env == ""
+      @warn "API secret details not found, authenticate manuallly with auth() \n"
+    end
+  
+  # added a condition to either take the keys that are set in the ENV else revert 
+  # to the user keys that the user places in variables/input into the function
+    if api_key_env != "" || api_secret_env != ""
+      print("ENV key is set begin auth() \n")
+      auth(api_key_env, api_secret_env)
+    elseif api_key_env == "" || api_secret_env == ""
+      print("Authenticate manually - ENV key is not set \n")
+      auth(api_key, api_secret)
     end
 
-    auth(api_key, api_secret)
     return
   end
 
