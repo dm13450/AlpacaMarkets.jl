dbda72d386d1e05a7a99d963c93108d8f0eb8a3c
dm13450 <dean.markwick@talk21.com>
2023-06-11 08:28:57 +0100

    Update tests

--- test/orders_test.jl
+++ test/orders_test.jl
@@ -2,12 +2,12 @@
 
   @testset "Req fields" begin
 
-    params = create_req_fields(symbol="AAPL", side = "buy", qty = 100, time_in_force = 100, type = "market")
+    params = AlpacaMarkets.create_order_params(symbol="AAPL", side = "buy", qty = 100, time_in_force = "ioc", type = "market")
 
     @test params["qty"] == 100
     #@test !(["notional", "client_order_id"] in keys(params))
 
-    params = create_red_filed(symbol="AAPL", side = "buy", notional = 100, time_in_force = 100, type = "market")
+    params = AlpacaMarkets.create_order_params(symbol="AAPL", side = "buy", notional = 100, time_in_force = "ioc", type = "market")
 
     @test params["notional"] == 100
     #@test !(["qty", "client_order_id"] in keys(params))
